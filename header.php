7df24fa7bed0df7347391a33ae1fc101fad9e2fa
Rinacoder <iraledy@yandex.ru>
2021-12-15 18:51:36 +0300

    Разделила на отдельные файлы

---
+++ header.php
@@ -0,0 +1,15 @@
+<header id="header" class="header">
+  <img src="img/menu3.svg" class="imenu" id="imenu">
+  		<a href="https://rinacoder.github.io/films/index.html" class="logo">
+  			<img src="img/logo.svg" class="ilogo">
+  			
+  			<h1>
+  				moviereview
+  			</h1>
+  		</a>
+  		<div class="batton_reg">
+  			<input type="button" id="popup_r_b" class="button" value="регистрация">
+  		<input type="button" id="popup_e_b" class="button" value="вход">
+  		</div>
+
+  </header>
